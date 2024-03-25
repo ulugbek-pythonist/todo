adfe616f0fd5f5bd20f184dd9ad40ae0d36cd218
ulugbekpy <ulugbekpycpp@gmail.com>
2024-03-25 08:39:32 +0500

    bug fixes

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('adminkapro/', admin.site.urls),
     path('volt/', include('admin_volt.urls')),
     path('', include('task.urls')),
+    path("static/", serve,{'document_root': settings.MEDIA_ROOT}), 
+    path("media/", serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 handler404 = "task.views.custom_page_not_found"
